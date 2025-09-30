0ff8a3bd715cdf6ff7ad252216c948e4336edb0b
student1287-netizen <student1287@sch192.net>
2025-09-30 16:46:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+
+token = 'TeleBot'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
+
+ app.add_handler(CommandHandler("start", start))
