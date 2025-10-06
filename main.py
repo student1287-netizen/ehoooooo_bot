f1fd6f24937cab71dfcb87c6b66f51d0e150726f
student1287-netizen <student1287@sch192.net>
2025-10-06 18:12:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import telebot
 
-token = 'TeleBot'
+token = 'telebot'
 bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Здорова, брат! Я бот.")
+
+@bot.message_handler(commands=['help'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Так и быть, я постораюсь тебе помочь")
+
+@bot.message_handler(commands=['command2'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Ты молодец" or "Ты лучший")
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
+
 if __name__ == '__main__':
      bot.infinity_polling()
-
- app.add_handler(CommandHandler("start", start))
